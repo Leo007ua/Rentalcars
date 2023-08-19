51f400b3317c54fe3414f1325aa428c089d10973
Leo <139163325+Leo007ua@users.noreply.github.com>
2023-08-19 23:47:30 +0300

    Update ButtonHeart.jsx

--- src/components/Button/ButtonHeart.jsx
+++ src/components/Button/ButtonHeart.jsx
@@ -1,16 +1,17 @@
 import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai';
+import { StyledBtn } from './ButtonStyled';
 
 
 export const ButtonHeart = ({ onBtnHeartClick, heartColor, isFavorite }) => {
   return (
-    <button type="button" onClick={onBtnHeartClick}>
+    <StyledBtn type="button" onClick={onBtnHeartClick}>
       {isFavorite ? (
-        <AiFillHeart size={18} color="var(--blue-hover)" />
+        <AiFillHeart size={18} color="rgba(52, 112, 255, 1)" />
       ) : !heartColor ? (
-        <AiOutlineHeart size={18} color="var(--white)" />
+        <AiOutlineHeart size={18} color="rgba(67, 63, 63, 0.8)" />
       ) : (
-        <AiFillHeart size={18} color="var(--blue-hover)" />
+        <AiFillHeart size={18} color="rgba(52, 112, 255, 1)" />
       )}
-    </button>
+    </StyledBtn>
   );
 };
